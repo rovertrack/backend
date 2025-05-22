67103e9b079029a906a44211b5d1ce735274e495
rovertrack <160643895+rovertrack@users.noreply.github.com>
2025-05-22 12:54:27 +0530

    Update 2025_05_03_182513_create_posts_table.php

--- database/migrations/2025_05_03_182513_create_posts_table.php
+++ database/migrations/2025_05_03_182513_create_posts_table.php
@@ -17,6 +17,29 @@ return new class extends Migration
             $table->text('content');
             $table->timestamps();
         });
+         Schema::create('nurses', function (Blueprint $table) {
+            $table->foreignId('id')->constrained('users')->onDelete('cascade');
+            $table->string('qualification');
+            $table->integer('experience_years');
+            $table->text('bio');
+            $table->timestamps();
+        });
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_name');
+            $table->text('service_description');
+            $table->timestamps();
+        });
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('nurse_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->enum('booking_type', ['instant', 'long_term', 'caregiver']);
+            $table->enum('status', ['pending', 'ongoing', 'completed', 'cacelled']);
+            $table->dateTime('scheduled_time');
+            $table->timestamps();
+        });
 
         // Schema::create()
     }
